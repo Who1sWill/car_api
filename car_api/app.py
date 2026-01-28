eb0f75b8353886e15f7d3430f63df8c729ee7d86
Who1sWill <127344760+Who1sWill@users.noreply.github.com>
2026-01-28 10:41:32 -0300

    add router on fastapi

--- car_api/app.py
+++ car_api/app.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+
+from car_api.routers import users
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {'message': 'Olá Mundo!'}
+app.include_router(
+    router=users.router,
+    prefix='/api/v1/users', #Boas práticas de REST '/api/v1/...', versionar endpoints
+    tags=['users'],
+)
+
+@app.get('/health_check', status_code=status.HTTP_200_OK)
+def health_check():
+    return {'status': 'Ok'}
